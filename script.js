3f773ff82fc2e0fce833547cf517ae7579b2b1a2
Fatima Ejaz <92662988+fatima2k18@users.noreply.github.com>
2025-07-02 12:38:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,30 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+function romanToInt(roman) {
+    const romanMap = {
+        'I': 1, 'V': 5, 'X': 10,
+        'L': 50, 'C': 100,
+        'D': 500, 'M': 1000
     };
 
-  //your code here
-	// Initialize an empty string to build the Roman numeral
-  let roman = "";
-  // Loop through each symbol-value pair
-  symbols.forEach((item) => {
-    // Destructure the array to get the symbol and its value
-    const [symbol, value] = item;
-    // While the current number is greater than or equal to the current value
-    while (num >= value) {
-      // Add the corresponding Roman numeral symbol to the result
-      roman += symbol;
-      // Subtract the value from the number
-      num -= value;
+    let total = 0;
+
+    for (let i = 0; i < roman.length; i++) {
+        const curr = romanMap[roman[i]];
+        const next = romanMap[roman[i + 1]];
+
+        if (next > curr) {
+            total += next - curr;
+            i++; // skip next character
+        } else {
+            total += curr;
+        }
     }
-  });
-  // Return the complete Roman numeral string
-  return roman;
 
-
+    return total;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-console.log(convertToRoman(14)); // XIV
-console.log(convertToRoman(798)); // DCCXCVIII
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+// Example usage:
+const input = prompt("Enter a Roman numeral (e.g. XIV):");
+const result = romanToInt(input.toUpperCase());
+alert("The integer value is: " + result);
 
-console.log(convertToRoman(36));
-
-// // do not edit below this line
-module.exports = convertToRoman;
-// console.log(convertToRoman(36));
 
